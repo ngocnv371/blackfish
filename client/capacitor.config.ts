5bddf8fd536a12cb5a9ea192c77d694b00f12c9d
Ngoc Ngo <ngocnv371@gmail.com>
2022-08-06 14:38:17 +0700

    add environments

--- client/capacitor.config.ts
+++ client/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.blackfish.client',
   appName: 'blackfish',
   webDir: 'build',
   bundledWebRuntime: false
